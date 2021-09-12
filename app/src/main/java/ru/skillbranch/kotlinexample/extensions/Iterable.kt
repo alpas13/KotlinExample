03c55e89bfef2255f523f28846dcfb70990272ae
Alexey <alexey@Oleksiys-MBP.fritz.box>
2021-09-12 23:04:31 +0200

    Removed iterable extantion function

--- app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
@@ -1,16 +1,2 @@
 package ru.skillbranch.kotlinexample.extensions
 
-fun List<String>.dropLastUntil(conditions: String): List<String> {
-    var counter = 0
-    var notMatchConditions = true
-    val tempList = mutableListOf<String>()
-
-    while (counter < this.size && notMatchConditions) {
-        if (this[counter] != conditions) {
-            tempList.add(this[counter])
-            counter++
-        } else notMatchConditions = false
-    }
-
-    return tempList
-}
